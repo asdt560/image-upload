e9f0a21d40a615ef18a33de7f9c13e9ede3ea66a
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-04-05 17:35:16 -0300

    Update Main,js

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,5 +1,14 @@
+import React, { useState, useEffect } from 'react'
 
 const Main = () => {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/mainimage")
+      .then((res) => res.json())
+      .then((data) => setImage(data.message));
+  }, []);
+
   return (
     <h1>Hello World</h1>
   )
