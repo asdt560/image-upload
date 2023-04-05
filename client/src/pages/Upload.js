999137fb8d6c5562f83397c93f8b0bc820b04748
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-04-05 18:07:55 -0300

    Create upload form

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,6 +1,14 @@
 const Upload = () => {
   return (
-    <h1>Upload image to server</h1>
+    <main>
+      <h1>Upload image to server</h1>
+      <form action="upload" enctype="multipart/form-data" method="post">
+        <input aria-label="title" type="text" name="title" />
+        <input aria-label="category" type="text" name="category" />
+        <input aria-label="files" type="file" name="multipleFiles" multiple="multiple" />
+        <input type="submit" value="Upload" />
+      </form>
+    </main>
   )
 }
 
