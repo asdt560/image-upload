7346b6274cb52704209beb762eb097c115aafc87
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-04-05 17:32:41 -0300

    Add test image

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const port = 8000
+
+app.get('/', (req, res) => {
+  const options = {
+    root: __dirname
+  }
+
+  res.sendFile('./images/testimg/foto.jpg')
+})
 
 app.listen(port, () => {
   console.log(`Server running in ${port}`)
 })
-app.use(cors());
-app.use(express.json());
