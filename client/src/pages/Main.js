323ee64fa29cbbc254b30aeab58c83a876dae24b
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-04-06 17:42:27 -0300

    Remove temporary main page request

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -3,15 +3,9 @@ import React, { useState, useEffect } from 'react'
 const Main = () => {
   const [image, setImage] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/mainimage")
-      .then((res) => setImage(res.url))
-  }, []);
-
   return (
     <main>
       <h1>Hello World</h1>
-      <img src={image} alt="placeholder" />
     </main>
   )
 }
