61ccdd77fae14393b70086b9d3bc5179af471bd0
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-03-14 14:52:46 -0300

    Add categories list page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,7 @@ const Navbar = () => {
   return (
     <nav>
       <NavLink to="/">Main</NavLink>
+      <NavLink to="/categories">Categories</NavLink>
       <NavLink to="/upload">Add Image</NavLink>
       <NavLink to="/add_category">Add Category</NavLink>
     </nav>
