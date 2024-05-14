10e130a74a45ec6e8dda3f9abf3a3c49ed9f70f2
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-05-14 18:16:22 -0300

    Add conditional render to all links dependent on login status

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,56 +1,15 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { checkSession, destroySession } from '../redux/session/sessionSlice';
 
-const RightEnd = () => {
+const Navbar = () => {
   const dispatch = useDispatch()
   const user = useSelector((state) => state.sessionReducer.user)
 
   const logout = async () => {
     dispatch(destroySession())
   }
-
-  const getUserData = async () => {
-    const data = await dispatch(checkSession())
-    console.log(data.payload.user)
-  }
-
-  useEffect(() => {
-    console.log(user, 'user')
-    getUserData()
-  }, [])
-
-  if (user) {
-    return (
-      <div
-        onClick={() => logout()}
-        className="py-2 px-8 pointer font-bold text-lg text-white 
-        font-sans hover:bg-white hover:text-gray-900">
-        {user.username}
-      </div>
-    )
-  } else {
-    return (
-      <div className="flex justify-between bg-gray-900">
-        <NavLink
-          className="py-2 px-8 pointer font-bold text-lg text-white 
-          font-sans hover:bg-white hover:text-gray-900"
-          to="/signup"
-        >Sign Up</NavLink>
-        <NavLink
-          className="py-2 px-8 pointer font-bold text-lg text-white 
-          font-sans hover:bg-white hover:text-gray-900"
-          to="/login"
-        >Log In</NavLink>
-      </div>
-    )
-  }
-}
-
-const Navbar = () => {
-
-  const user = useSelector((state) => state.sessionReducer.user)
 
   const getUserData = async () => {
     const data = await dispatch(checkSession())
@@ -71,15 +30,37 @@ const Navbar = () => {
         className="py-2 px-8 pointer font-bold text-lg text-white font-sans hover:bg-white hover:text-gray-900"
         to="/categories"
       >Categories</NavLink>
-      <NavLink
-        className="py-2 px-8 pointer font-bold text-lg text-white font-sans hover:bg-white hover:text-gray-900"
-        to="/upload"
-      >Add Image</NavLink>
-      <NavLink
-        className="py-2 px-8 pointer font-bold text-lg text-white font-sans hover:bg-white hover:text-gray-900"
-        to="/add_category"
-      >Add Category</NavLink>
-      <RightEnd />
+      {user ?
+        <>
+          <NavLink
+            className="py-2 px-8 pointer font-bold text-lg text-white font-sans hover:bg-white hover:text-gray-900"
+            to="/upload"
+          >Add Image</NavLink>
+          <NavLink
+            className="py-2 px-8 pointer font-bold text-lg text-white font-sans hover:bg-white hover:text-gray-900"
+            to="/add_category"
+          >Add Category</NavLink>
+          <div
+            onClick={() => logout()}
+            className="py-2 px-8 pointer font-bold text-lg text-white 
+          font-sans hover:bg-white hover:text-gray-900"
+          >
+            {user.username}
+          </div>
+        </>
+        :
+        <div className="flex justify-between bg-gray-900">
+          <NavLink
+            className="py-2 px-8 pointer font-bold text-lg text-white 
+          font-sans hover:bg-white hover:text-gray-900"
+            to="/signup"
+          >Sign Up</NavLink>
+          <NavLink
+            className="py-2 px-8 pointer font-bold text-lg text-white 
+          font-sans hover:bg-white hover:text-gray-900"
+            to="/login"
+          >Log In</NavLink>
+        </div>}
     </nav>
   )
 }
