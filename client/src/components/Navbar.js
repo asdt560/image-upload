2df243fb3e5e201744c103a5941b23c5647e38e8
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-05-21 19:09:20 -0300

    Set up dropdown with tailwindUI

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useEffect } from 'react'
+import { Fragment } from 'react'
+import { Menu, MenuButton, MenuItems, MenuItem, Transition } from '@headlessui/react'
 import { useDispatch, useSelector } from 'react-redux';
 import { checkSession, destroySession } from '../redux/session/sessionSlice';
 
@@ -43,13 +45,38 @@ const Navbar = () => {
             className="py-2 px-8 pointer font-bold text-lg text-white font-sans hover:bg-white hover:text-gray-900"
             to="/add_category"
           >Add Category</NavLink>
-          <div
-            onClick={() => logout()}
+          <Menu
+            as="div"
             className="py-2 px-8 pointer font-bold text-lg text-white 
-          font-sans hover:bg-white hover:text-gray-900"
+            font-sans hover:bg-white hover:text-gray-900"
           >
-            {user.username}
-          </div>
+            <MenuButton>
+              {user.username}
+            </MenuButton>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                    <MenuItem>
+                        <button
+                          className='bg-gray-100 text-gray-900 block 
+                          w-full px-4 py-2 text-left text-sm'
+                          onClick={() => logout()}
+                        >
+                          Sign out
+                        </button>
+                    </MenuItem>
+                </div>
+              </MenuItems>
+            </Transition>
+          </Menu>
         </>
         :
         <div className="flex justify-between bg-gray-900">
