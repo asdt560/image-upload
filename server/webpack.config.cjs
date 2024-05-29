8628b577dd2184532156b89fdafee3de222af18b
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-05-29 18:05:22 -0300

    Fix pg-native issue

--- server/webpack.config.cjs
+++ server/webpack.config.cjs
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './server.js',
+  plugins: [
+    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/',
