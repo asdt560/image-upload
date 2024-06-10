4551312fafc6bbfc7548f63d614ceb42fd5107f3
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-06-10 19:03:11 -0300

    Add jest-fetch-mock and audit packages

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders title', () => {
+  render(
+    <Provider store={store}>  
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/See images here/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('renders navbar', () => {
+  it('renders', () => {
+    render(
+      <Provider store={store}>  
+        <App />
+      </Provider>
+    )
+    const navBar = screen.getByText(/logo/i)
+    expect(navBar).toBeInTheDocument();
+  })
 });
