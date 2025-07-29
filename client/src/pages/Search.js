40f93ed9d56e7840ec5ad7f050541d4d730ac8be
JustoRago <103001263+asdt560@users.noreply.github.com>
2025-07-29 20:24:45 -0300

    Display images found

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -12,6 +12,14 @@ const Search = () => {
     <main>
       <h1>Find image by name</h1>
       <input type="text" onChange={handleSearch}></input>
+      {images.length &&
+      images.map((image) => (
+        <div className="flex flex-col items-center">
+          <p className='text-1xl text-white font-bold'>{image.img_name}</p>
+          <p className='text-1xl text-white font-bold'>{image.created_at}</p>
+          <img className="w-full rounded-sm" key={image.id} alt='' src={`http://localhost:5000/${image.filepath}`}/>
+        </div>
+      ))}
     </main>
   )
 }
