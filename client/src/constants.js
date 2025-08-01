bcfd300fce7726e565f2b3494908416fe18963ae
JustoRago <103001263+asdt560@users.noreply.github.com>
2025-08-01 19:47:52 -0300

    Add more constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,8 @@
 export const mainClass = 'flex flex-col items-center gap-8';
 export const h1Class = 'text-2xl text-white font-bold';
 export const formClass = "flex gap-4 flex-col items-center";
+export const labelClass = "w-full text-white font-bold";
+export const inputClass = (errors) => `p-2 rounded-md border-2 cursor-pointer 
+  ${errors.email ? "border-red-400 text-red-500" : "bg-gray-800 text-white"} border-gray-400  w-full`;
+export const errorPClass = "text-red-500 p-1 text-xs";
+export const submitButtonClass = "w-full p-2 border-4 border-white border-double rounded-md text-white font-bold text-lg bg-gray-700"
