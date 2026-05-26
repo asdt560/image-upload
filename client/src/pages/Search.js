2457db6693765fd3e1e0b1d3f52a347c54a29c97
JustoRago <103001263+asdt560@users.noreply.github.com>
2026-05-26 18:43:13 -0300

    Place search results in box

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -13,7 +13,7 @@ const Search = () => {
   const handleSearch = async () => {
     const results = await dispatch(searchImages(query))
     console.log(results, query, "SEARCH RESULT")
-    if(results.payload) setImages(results.payload.body)
+    if (results.payload) setImages(results.payload.body)
   }
 
   useEffect(() => {
@@ -22,19 +22,21 @@ const Search = () => {
   return (
     <main className={mainClass}>
       <h1 className={h1Class}>Results for: {query}</h1>
-      {
-        images.length ?
-          images.map((image) => (
-            <div className="flex flex-col items-center">
-              <p className={pClass}>{image.img_name}</p>
-              <p className={pClass}>{image.created_at}</p>
-              <img className="w-full rounded-sm" key={image.id} alt='' src={`http://localhost:5000/${image.filepath}`} />
-            </div>
-          )) :
-          <p className={pClass}>
-            No Results Found
-          </p>
-      }
+      <div className="flex flex-col gap-2 items-center">
+        {
+          images.length ?
+            images.map((image) => (
+              <div className="flex items-center">
+                <p className={pClass}>{image.img_name}</p>
+                <p className={pClass}>{image.created_at}</p>
+                <img className="w-full rounded-sm" key={image.id} alt='' src={`http://localhost:5000/${image.filepath}`} />
+              </div>
+            )) :
+            <p className={pClass}>
+              No Results Found
+            </p>
+        }
+      </div>
     </main>
   )
 }
